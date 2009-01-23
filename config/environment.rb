90baaa55181ab0e3ecab0a96974c8587be16a886
Stefan Penner <stefan.penner@gmail.com>
2009-01-23 12:04:50 -0600

    removed haml a a config gem dependancy

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,6 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "haml"
   config.gem 'mocha'
   config.gem 'thoughtbot-shoulda', :lib => 'shoulda',
                                    :source => "http://gems.github.com" 
