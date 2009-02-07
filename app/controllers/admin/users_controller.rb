eb43e611f585eae348e33e9ce98a8daf0a0746d1
Burke Libbey <burke@burkelibbey.org>
2009-02-07 16:57:53 -0600

    added password to AS forms

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,8 @@
 class Admin::UsersController < AdminController
   active_scaffold :users do |config|
-    config.columns = [:email, :roles, :confirmed]
+    config.columns = [:email, :roles, :confirmed, :password, :password_confirmation]
+    config.update.columns << [:password, :password_confirmation]
+    config.create.columns << [:password, :password_confirmation]
     config.list.columns = [:email, :roles, :confirmed]
     config.columns[:roles].form_ui = :select
     config.columns[:email].set_link :show
