585457372c1ce5713d104e7c4985864d73d86bfe
Stefan Penner <stefan.penner@gmail.com>
2009-02-07 17:24:48 -0600

    added set password and password_confirmation to password fields in the admin/users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,8 @@ class Admin::UsersController < AdminController
     config.columns = [:email, :roles, :confirmed, :password, :password_confirmation]
     config.update.columns << [:password, :password_confirmation]
     config.create.columns << [:password, :password_confirmation]
+    config.columns[:password].form_ui = :password
+    config.columns[:password_confirmation].form_ui = :password
     config.list.columns = [:email, :roles, :confirmed]
     config.columns[:roles].form_ui = :select
     config.columns[:email].set_link :show
