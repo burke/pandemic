6cbe3656491c2ef33e71fb6bd24f10ad22e7cb3d
Stefan Penner <stefan.penner@gmail.com>
2009-02-10 23:18:52 -0600

    seems the static sweeper was ommited for the last commit... its included this time

---
+++ app/sweepers/static_sweeper.rb
@@ -0,0 +1,22 @@
+class StaticSweeper < ActionController::Caching::Sweeper
+
+  def self.sweep
+   blacklist = %W{ 404.html
+                   422.html
+                   500.html
+                   blank.html}
+   
+   blacklist.map! { |file| RAILS_ROOT + "/public/"+file }               
+   cached = Dir[ "#{RAILS_ROOT}/public/**.html"]
+   
+   cached -= blacklist
+   
+   cached.each do |file|
+     FileUtils.rm_r(file) rescue Errno::ENOENT
+     message = "[ deleted ] #{file}" 
+     puts message
+     RAILS_DEFAULT_LOGGER.info(message)
+   end 
+  end
+end
+
